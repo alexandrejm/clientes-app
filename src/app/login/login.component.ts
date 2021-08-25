15398294b5cecc343090b6ff6122012441790a32
Alexandre <alexandrejm2004@yahoo.com.br>
2021-08-25 00:35:45 -0300

    Criação da tela de login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  username: string;
+  password: string;
+  loginError: boolean;
+  cadastrando: boolean;
+
+  constructor(
+    private router: Router
+  ) { }
+
+  onSubmit(){
+    this.router.navigate(['/home'])
+  }
+
+  preparaCadastrar(event){
+    event.preventDefault(); //Evita que o clique leve para outra página do href. O objetivo é alterar o título do login
+    this.cadastrando = true;
+  }
+
+  cancelaCadastro(){
+    this.cadastrando = false;
+  }
+
+}
