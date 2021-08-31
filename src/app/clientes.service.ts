c237dfaf40f47cff7a7c5bb8e7b254146ac4ba2d
Alexandre <alexandrejm2004@yahoo.com.br>
2021-08-30 21:06:56 -0300

    Edição e Leitura dados API

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -3,24 +3,39 @@ import { HttpClient } from '@angular/common/http'
 
 import { Cliente } from './clientes/cliente';
 import { Observable } from 'rxjs';
+import { calcPossibleSecurityContexts } from '@angular/compiler/src/template_parser/binding_parser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  constructor( private http: HttpClient) { 
-    
-  }
+  constructor( private http: HttpClient) {}
 
   salvar( cliente: Cliente ) : Observable<Cliente> {  //Em vez de <Cliente> pode-se usar <any>
     return this.http.post<Cliente>('http://localhost:8080/api/clientes' , cliente)
   }
 
-  getCliente() : Cliente { // Não precisa colocar a tipagem Cliente se não quiser
-    let cliente : Cliente = new Cliente();
-    cliente.nome = 'Fulano';
-    cliente.cpf = '8888888888';
-    return cliente;
+  atualizar( cliente: Cliente ) : Observable<any> {  //Usou <any> pq na resposta o backend nao retorna nada
+    return this.http.put<Cliente>(`http://localhost:8080/api/clientes/${cliente.id}` , cliente)
   }
+
+  getClientes() :  Observable<Cliente[]> {
+    return this.http.get<Cliente[]>('http://localhost:8080/api/clientes');
+  }
+
+  getClienteById(id: number) : Observable<Cliente> {
+    return this.http.get<any>(`http://localhost:8080/api/clientes/${id}`);
+  }
+
+  /* Clientes Manual
+  getClientes() : Cliente[] {
+    let cliente = new Cliente();
+    cliente.id = 1;
+    cliente.nome = 'Fulano';
+    cliente.dataCadastro = '18/04/2020';
+    cliente.cpf = '12345678900';
+    return[cliente]
+  } 
+  */
 }
--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { Cliente } from '../cliente'
 import { ClientesService } from '../../clientes.service'
@@ -11,20 +12,65 @@ import { ClientesService } from '../../clientes.service'
 export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
+  success: boolean = false;
+  errors: String[];
+  id: number;
 
-  constructor( private service: ClientesService) {
+  constructor(
+    private service: ClientesService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
     this.cliente = new Cliente();
-   }
-
-  ngOnInit(): void {
   }
 
-  onSubmit(){
-    this.service
-    .salvar(this.cliente)
-    .subscribe( response => {
-      console.log(response);
-    } )
+  ngOnInit(): void {
+    let params = this.activatedRoute.params;
+    if (params && params.value && params.value.id) { // Quer dizer que passou o id
+      this.id = params.value.id;
+      this.service
+        .getClienteById(this.id)
+        .subscribe(response => this.cliente = response,
+          errorResponse => this.cliente = new Cliente() // Ser nao houver cliente, nao faz nada. Instancia novo cliente
+        )
+    }
+  }
+
+  voltarParaListagem() {
+    this.router.navigate(['/clientes-lista']);
+  }
+
+  onSubmit() {
+    if (this.id) {
+
+      this.service
+        .atualizar(this.cliente)
+        .subscribe(response => {
+          this.success = true;
+          this.errors = null;
+        }, errorResponse => {
+          this.errors = ['Erro ao atualizar o cliente.']
+        })
+
+    } else {
+      this.service
+        .salvar(this.cliente)
+        .subscribe(response => {
+          this.success = true;
+          this.errors = null;
+          this.cliente = response;
+        }, errorResponse => {
+          this.success = false;
+          this.errors = errorResponse.error.errors;
+          //console.log(this.errors);
+          //console.log(errorResponse.error.errors[0].defaultMessage);
+          //console.log(errorString.split(',' , 3) );
+          //console.log(this.errors[0]);
+          //console.log(this.errors.find( ({ defaultMessage }) => defaultMessage === 'O campo CPF é obrigatório.' ));
+          //console.log(errorResponse.error.errors)
+        })
+    }
+
   }
 
 }
